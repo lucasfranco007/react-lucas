5269dc6e9377a6920cb1e62870c1b6f55be5dd3c
Lucas Franco <lucasfranco_1997@outlook.com>
2021-12-05 22:07:19 -0300

    navbar modificado

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -12,7 +12,6 @@ const navbar = () => {
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
         <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Inicio</a>
         <a className="nav-link" href="#">Productos</a>
         <a className="nav-link disabled">Galeria</a>
       </div>
